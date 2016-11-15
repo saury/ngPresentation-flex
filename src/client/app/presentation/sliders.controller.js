711e898c4a29e297e6505e00a0d88f4650ae6782
Jin Xu <ext.jin.xu@CNSHHQ-0604.ef.com>
2016-11-15 16:13:52 +0800

    finish the same example part for flex

--- src/client/app/presentation/sliders.controller.js
+++ src/client/app/presentation/sliders.controller.js
@@ -17,12 +17,32 @@
                 'quarter #4'
             ],
             showLong: function(order){
-                if(order > 0) return
+                if(order > 1) return
+                else if(order === 1) {this.quaters.shift(); return}
                 this.quaters[order] = 'long long long long'
             },
             appendChild: function(){
-                console.log('pooque');
-                this.quaters.push('quarter #n')
+                this.quaters.push('quarter')
+            }
+        }
+
+        vm.changeAIJA = {
+            AIVal: 'center',
+            ASVal: 'center',
+            JAVal: 'center',
+            FDVal: 'row',
+            FWVal: 'wrap',
+            AIList: ['center','flex-start','flex-end','baseline','stretch'],
+            ASList: ['center','flex-start','flex-end','baseline','stretch'],
+            JAList: ['center','flex-start','flex-end','space-between','space-around'],
+            FDList: ['row', 'row-reverse', 'column', 'column-reverse'],
+            FWList: ['nowrap', 'wrap', 'wrap-reverse'],
+            mode: 'noedit',
+            editMode: function() {
+                if(this.mode === 'noedit'){
+                    this.mode = 'edit'
+                }
+                else this.mode = 'noedit'
             }
         }
     }
